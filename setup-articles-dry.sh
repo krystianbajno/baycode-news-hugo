d28b2fd93f4c1e1972d9e253b589bf3ee7be4d39
Krystian Bajno <krystian.bajno@gmail.com>
2024-01-08 20:12:56 +0100

    Dry run

---
+++ setup-articles-dry.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -e "content/articles/.gitignore" ]; then
+    rm content/articles/.gitignore
+fi
+
+if [ -e "content/articles/README.md" ]; then
+    rm content/articles/README.md
+fi
+
+for dir in content/articles/*/; do
+    dir_name="${dir%/}"
+
+    if [ -e "$dir_name/README.md" ]; then
+        echo "+++" >> "$dir_name/index.md"
+        cat "$dir_name/frontmatter.toml" >> "$dir_name/index.md"
+        echo -e "\n+++\n" >> "$dir_name/index.md"
+        cat "$dir_name/README.md" >> "$dir_name/index.md"
+
+        rm "$dir_name/README.md"
+        rm "$dir_name/frontmatter.toml"
+
+        cat "$dir_name/index.md" | head -c 420
+    fi
+
+    if [ -e "$dir_name/src" ]; then
+        rm -rf "$dir_name/src"
+    fi
+    
+    echo "+ $dir_name"
+done
